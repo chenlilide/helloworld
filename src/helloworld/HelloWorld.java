cffec5f28e0b07a00a5df016d845a00a22b9bc22
Administrator <Administrator@10.115.168.172>
2019-04-20 14:01:53 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,6 +3,6 @@ package helloworld;
 public class HelloWorld {
  
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World°¡¹þ¹þ¹þ");
     }
 }
